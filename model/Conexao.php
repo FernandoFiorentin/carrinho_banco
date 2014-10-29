dbf7b99615594ab40826dacd0493fd3d79f570d2
Fernando Fiorentin <fernandoffiorentin@gmail.com>
2014-10-29 18:22:33 -0200

    Alterada classe de conexao

--- model/Conexao.php
+++ model/Conexao.php
@@ -1,17 +1,20 @@
 <?php
-class Conexao{
-	private $instance;
 
-	function __construct(){
+class Conexao {
 
-	}
+    public static $instance;
 
-	public function getInstance(){
-		if (!(isset($this->instance))){
-			$this->instance = new PDO("mysql:host=localhost;dbname=carrinho", "root", "");	
-		}
-		return $this->instance;
-	}
+    private function __construct() {
+        
+    }
+
+    public static function getInstance() {
+        if (!isset(self::$instance)) {
+            self::$instance = new PDO('mysql:host=localhost;dbname=chamados', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            self::$instance->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
+        } return self::$instance;
+    }
+
 }
-
 ?>
