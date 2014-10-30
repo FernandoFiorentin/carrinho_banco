6514e10a96272b1c7d4a54b67d10862c71770955
Fernando Fiorentin <fernandoffiorentin@gmail.com>
2014-10-30 21:37:51 -0200

    Alterado categoriaDAO add metodo buscarPorId()

--- model/categoriaDAO.php
+++ model/categoriaDAO.php
@@ -68,6 +68,26 @@ class CategoriaDAO {
         }
     }
 
+    public function buscarPorId($id) {
+        try {
+            $sql = 'SELECT idCategoria,
+                    nome,
+                    descricao,
+                    ativo                    
+                    FROM categoria where idCategoria = :idCategoria';
+
+            $stmt = Conexao::getInstance()->prepare($sql);
+            $stmt->bindValue(':idCategoria', $id);
+            $stmt->execute();
+
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            $cat = new Categoria($row['idCategoria'], $row['nome'], $row['descricao'], $row['ativo']);
+            return $cat;
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+
     public function deletar($idCategoria) {
         try {
             $sql = 'delete from categoria where idCategoria = :idCategoria';
@@ -78,32 +98,32 @@ class CategoriaDAO {
             echo 'erro ao deletar';
         }
     }
-}
 
+}
 
 /*
 
-calss CategoriaDAO
-{
-	function __construct()
-	{
-		include_once 'abreConexao.php';		
-		include_once 'categoria.php'
-	}
+  calss CategoriaDAO
+  {
+  function __construct()
+  {
+  include_once 'abreConexao.php';
+  include_once 'categoria.php'
+  }
 
-	public function insert(Categoria $categoria)
-	{
-		$stmt = $con->prepare(
-			'insert into categoria(nome, descricao, ativo) values(:nome, :descricao, :ativo)';
-		);
+  public function insert(Categoria $categoria)
+  {
+  $stmt = $con->prepare(
+  'insert into categoria(nome, descricao, ativo) values(:nome, :descricao, :ativo)';
+  );
 
-		$stmt->bindValue(':nome',$categoria->getNome());
-		$stmt->bindValue(':descricao',$categoria->getDescricao());
-		$stmt->bindValue(':ativo',$categoria->getAtivo());
+  $stmt->bindValue(':nome',$categoria->getNome());
+  $stmt->bindValue(':descricao',$categoria->getDescricao());
+  $stmt->bindValue(':ativo',$categoria->getAtivo());
 
-		$stmt->execute();
-		$stmt->commit();
-	}
+  $stmt->execute();
+  $stmt->commit();
+  }
 
-}*/
+  } */
 ?>
