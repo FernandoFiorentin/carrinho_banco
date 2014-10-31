5be21a83a0f48b69a59c57fbc37344e428a46c87
Fernando Fiorentin <fernandoffiorentin@gmail.com>
2014-10-31 21:54:09 -0200

    Ajustado categoriaDAO

--- model/categoriaDAO.php
+++ model/categoriaDAO.php
@@ -101,29 +101,4 @@ class CategoriaDAO {
 
 }
 
-/*
-
-  calss CategoriaDAO
-  {
-  function __construct()
-  {
-  include_once 'abreConexao.php';
-  include_once 'categoria.php'
-  }
-
-  public function insert(Categoria $categoria)
-  {
-  $stmt = $con->prepare(
-  'insert into categoria(nome, descricao, ativo) values(:nome, :descricao, :ativo)';
-  );
-
-  $stmt->bindValue(':nome',$categoria->getNome());
-  $stmt->bindValue(':descricao',$categoria->getDescricao());
-  $stmt->bindValue(':ativo',$categoria->getAtivo());
-
-  $stmt->execute();
-  $stmt->commit();
-  }
-
-  } */
 ?>
